84a43c974139915e1b29aaf4928fa37a896919a7
USER <kevinpb.py@gmail.com>
2025-01-05 16:29:55 +0100

    program version 2

--- program _version_2.py
+++ program _version_2.py
@@ -1,27 +1,44 @@
 from calculate_fns import calculate_basic_operations
+from calculate_fns import obtine_values
 
-
-result = int
+result = None
 
 while True is True:
+    
     # Option selection
+    
     print("Select an option: ")
+    
     print("1: Make an operation")
+    
     print("2: Continue working with the result")
+    
     print("3: Exit")
+    
     option = input()
+    
     try:
+        
         option = int(option)
+    
     except Exception:
+        
         print("You have entered an incorrect option")
+    
     if option == 1:
+    
+        #Getting the data    
+    
         try:
+        
             print("Enter the numbers and the symbol of the operation that you want to execute separated by espaces. As follows: 5 + 7")
         
             data_enter = input()
         
             data = data_enter.split(" ")
-
+    
+            #Checking data
+    
             if len(data) > 3:
             
                 print("You entered an unnecessary value")
@@ -33,31 +50,53 @@ while True is True:
                 data = data_enter.split(" ")
             
         except Exception:
+        
             print("You have entered an incorrect value")
     
 
         # Value recollection
+        
         try:
-            data[0] = int(data[0]) 
-
-            data[2] = int(data[2]) 
+        
+            obtine_values(data) 
+        
         except Exception:
+        
             print("You have entered an incorrect value") 
 
         try:
+        
            # Make an operation 
+        
             result = calculate_basic_operations(data)
+        
             print(calculate_basic_operations(data))
         
+        
         except ZeroDivisionError:
+        
             print("Cannot be divided by zero")
+        
         except Exception:
+        
             pass
 
-    elif option == 2:
+    #Checking for a result
+    
+    elif option == 2 and result == None:
+    
+        print("There is not a result to work")
+
+    elif option == 2 and result != None:
+        
+        #Getting data    
+        
         try:
+    
             print("Enter the symbol and the number separated by spaces to perform the operation with the result.")
+            
             result_list = []
+            
             result_list.append(result)
         
             data_enter = input()
@@ -65,8 +104,11 @@ while True is True:
             data = data_enter.split(" ")
         
             result_list.append(data[0])
+            
             result_list.append(data[1])
-
+            
+            #Checking data
+            
             if len(result_list) > 3:
             
                 print("You entered an unnecessary value")
@@ -74,6 +116,7 @@ while True is True:
                 print("Enter the numbers and the symbol of the operation that you want to execute separated by espaces. As follows: 5 + 7")
             
                 result_list = []
+    
                 result_list.append(result)
         
                 data_enter = input()
@@ -83,26 +126,34 @@ while True is True:
                 result_list.append(data)
 
         except Exception:
+    
             print("You have entered an incorrect value")
 
         try:
-            result_list[0] = int(result_list[0]) 
-
-            result_list[2] = int(result_list[2]) 
+            #Obtine values
+            
+            obtine_values(result_list)
+    
         except Exception:
+    
             print("You have entered an incorrect value") 
 
         try:
+            
+            #Make operations with the result
+            
             result = calculate_basic_operations(result_list)
+            
             print(calculate_basic_operations(result_list))
         
         except ZeroDivisionError:
+            
             print("Cannot be divided by zero")
     
+    #Break the cycle
     elif option == 3:
+        
         break
-    
-    else:
-        print("You have not placed a correct option")
+
     
     
