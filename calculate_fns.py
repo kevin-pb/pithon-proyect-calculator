7613f47503a6af890aee70dc14625c79efad6785
USER <kevinpb.py@gmail.com>
2025-01-05 01:41:54 +0100

    commit

---
+++ calculate_fns.py
@@ -0,0 +1,23 @@
+def calculate_basic_operations(array):
+    try:  
+        if array [1] == "+":
+            result = array[0] + array[2]
+            return(result)
+
+        if array [1] == "-":
+            result = array[0] - array[2]
+            return(result)
+
+        if array [1] == "*":
+            result = array[0] * array[2]
+            return(result)
+
+        if array [1] == "/":
+            result = array[0] / array[2]
+            return(result)
+    except ZeroDivisionError:
+        print("Cannot be divided by zero")
+
+    except Exception:
+        pass
+    
--- program _version_2.py
+++ program _version_2.py
@@ -1,58 +1,19 @@
+from calculate_fns import calculate_basic_operations
+
+
 result = int
-try:
-    print("Enter the numbers and the symbol of the operation that you want to execute separated by espaces. As follows: 5 + 7")
-    data_enter = input()
-    data = data_enter.split(" ")
-    print(data)
-
-    if len(data) > 3:
-        print("You entered an unnecessary value")
-        print("Enter the numbers and the symbol of the operation that you want to execute separated by espaces. As follows: 5 + 7")
-        data_enter = input()
-        data = data_enter.split(" ")
-        print(data)
-        
-except Exception:
-    print("You have entered an incorrect value")
-    
-
-try:
-    data[0] = int(data[0]) 
-
-    data[2] = int(data[2]) 
-except ValueError:
-    print("You have entered an incorrect value") 
-
-try:
-    if data [1] == "+":
-        result = data[0] + data[2]
-        print(result)
-
-    if data [1] == "-":
-        result = data[0] - data[2]
-        print(result)
-
-    if data [1] == "*":
-        result = data[0] * data[2]
-        print(result)
-
-    if data [1] == "/":
-        result = data[0] / data[2]
-        print(result)
-except ZeroDivisionError:
-    print("Cannot be divided by zero")
-
-except Exception:
-    pass
 
 while True is True:
-    print("Select a option: ")
-    print("1: Another operation")
+    # Option selection
+    print("Select an option: ")
+    print("1: Make an operation")
     print("2: Continue working with the result")
     print("3: Exit")
     option = input()
-    option = int(option)
-    
+    try:
+        option = int(option)
+    except Exception:
+        print("You have entered an incorrect option")
     if option == 1:
         try:
             print("Enter the numbers and the symbol of the operation that you want to execute separated by espaces. As follows: 5 + 7")
@@ -75,34 +36,25 @@ while True is True:
             print("You have entered an incorrect value")
     
 
+        # Value recollection
         try:
             data[0] = int(data[0]) 
 
             data[2] = int(data[2]) 
-        except ValueError:
+        except Exception:
             print("You have entered an incorrect value") 
 
         try:
-            if data [1] == "+":
-                result = data[0] + data[2]
-                print(result)
-
-            if data [1] == "-":
-                result = data[0] - data[2]
-                print(result)
-
-            if data [1] == "*":
-                result = data[0] * data[2]
-                print(result)
-
-            if data [1] == "/":
-                result = data[0] / data[2]
-                print(result)
+           # Make an operation 
+            result = calculate_basic_operations(data)
+            print(calculate_basic_operations(data))
         
         except ZeroDivisionError:
             print("Cannot be divided by zero")
+        except Exception:
+            pass
 
-    if option == 2:
+    elif option == 2:
         try:
             print("Enter the symbol and the number separated by spaces to perform the operation with the result.")
             result_list = []
@@ -114,7 +66,7 @@ while True is True:
         
             result_list.append(data[0])
             result_list.append(data[1])
-            print(result_list)
+
             if len(result_list) > 3:
             
                 print("You entered an unnecessary value")
@@ -129,7 +81,7 @@ while True is True:
                 data = data_enter.split(" ")
 
                 result_list.append(data)
-                print(result_list)
+
         except Exception:
             print("You have entered an incorrect value")
 
@@ -137,30 +89,20 @@ while True is True:
             result_list[0] = int(result_list[0]) 
 
             result_list[2] = int(result_list[2]) 
-        except ValueError:
+        except Exception:
             print("You have entered an incorrect value") 
 
         try:
-            if result_list [1] == "+":
-                result = result_list[0] + result_list[2]
-                print(result)
-
-            if result_list [1] == "-":
-                result = result_list[0] - result_list[2]
-                print(result)
-
-            if result_list [1] == "*":
-                result = result_list[0] * result_list[2]
-                print(result)
-
-            if result_list [1] == "/":
-                result = result_list[0] / result_list[2]
-                print(result)
+            result = calculate_basic_operations(result_list)
+            print(calculate_basic_operations(result_list))
         
         except ZeroDivisionError:
             print("Cannot be divided by zero")
     
-    if option == 3:
+    elif option == 3:
         break
+    
+    else:
+        print("You have not placed a correct option")
     
     
